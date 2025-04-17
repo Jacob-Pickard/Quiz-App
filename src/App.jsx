0be7383af951726a4ec0872bfedad4b954a94b1b
Jacob Pickard <pickard7477j@yahoo.com>
2025-04-17 12:21:16 -0700

    Router fixes for ghpages, additional blogposts relating to Q3 research assignments

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { CssBaseline, Box, Button, Typography, Grid } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import QuizPage from './components/QuizPage';
@@ -40,7 +40,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename="/Quiz-App">
+      <Router>
         <Box className="app-container">
           <Typography variant="h2" gutterBottom>
             <Link to="/" className="title-link">
